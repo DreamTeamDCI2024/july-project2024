a9682a9aa2944d341dbc6d60eca1f971a612af57
Helen <iamhelena13@gmail.com>
2024-07-16 10:25:36 +0200

    Server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Hi");
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Initialize the app
+const app = express();
+
+// PORT
+const PORT = process.env.PORT;
+
+//Use the json middleware
+app.use(express.json());
+
+// Paths
+app.get('/hello', (req, res) => {
+    console.log(req);
+
+    res.send('Hello, World!');
+})
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
