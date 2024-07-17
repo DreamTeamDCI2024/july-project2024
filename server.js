51e956c2a3e80c7634e7bcca765c6ee0695033aa
Helen <iamhelena13@gmail.com>
2024-07-17 15:50:56 +0200

    changed server and .env

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -9,16 +10,26 @@ const app = express();
 // PORT
 const PORT = process.env.PORT || 4000;
 
-//Use the json middleware
-app.use(express.json());
+// Use the json middleware
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb", extended: true }));
 
 // Paths
 app.get('/hello', (req, res) => {
     console.log(req);
 
-    res.send('Hello, World!');
+    req.setEncoding("Hello");
 })
 
+// Listening server
+mongoose
+    .connect(process.env.CONNECTION_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on ${PORT}`);
+        });
+    })
+    .catch((error) => console.log(error));
 
-//Listening server
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+console.log('PORT:', process.env.PORT);
+console.log('CONNECTION_URL:', process.env.CONNECTION_URL);
